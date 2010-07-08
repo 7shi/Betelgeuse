bf92e8905bcf971071aa4b6f399a229d9cfde5d7
7shi <7shi@live.jp>
2010-07-08 14:57:27 +0900

    use Windows Forms

--- Test/Program.fs
+++ Test/Program.fs
@@ -1,22 +1,37 @@
 ﻿open System
+open System.Drawing
 open System.IO
 open System.Text
-open Microsoft.Win32
+open System.Windows.Forms
 
 open ELF
 
-Console.WriteLine("*** Betelgeuse - Alpha Orionis ***")
-Console.WriteLine()
+let main() =
+    let f = new Form()
+    let mono = new Font(FontFamily.GenericMonospace, f.Font.Size)
+    let t = new TextBox(Multiline = true, ScrollBars = ScrollBars.Both,
+                        Dock = DockStyle.Fill, WordWrap = false, Font = mono)
+    let m = new MainMenu()
+    let mi = new MenuItem("開く")
+    mi.Click.Add <| fun _ ->
+        use ofd = new OpenFileDialog()
+        if ofd.ShowDialog() = DialogResult.OK then
+            use fs = new FileStream(ofd.FileName, FileMode.Open)
+            use br = new BinaryReader(fs)
+            let sb = new StringBuilder()
+            ignore <| sb.AppendLine("*** Betelgeuse - Alpha Orionis ***")
+            ignore <| sb.AppendLine()
+            try
+                ignore <| ELF64.Read sb br
+            with ex ->
+                ignore <| sb.AppendLine(ex.Message)
+            t.Text <- sb.ToString()
+    ignore <| m.MenuItems.Add mi
+    f.Menu <- m
+    f.Controls.Add t
+    f.Show()
+    
+    Application.Run(f)
 
-let ofd = new OpenFileDialog()
-let result = ofd.ShowDialog()
-if result.HasValue && result.Value then
-    use fs = ofd.OpenFile()
-    use br = new BinaryReader(fs)
-    let sb = new StringBuilder()
-    try
-        ignore <| ELF64.Read sb br
-    with ex ->
-        ignore <| sb.Append(ex.Message)
-    Console.WriteLine(sb.ToString())
-    Console.ReadLine() |> ignore
+[<STAThread>]
+do main()
