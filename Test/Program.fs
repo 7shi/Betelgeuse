7d55931da886c43f86e6ac223ca878bb410e681c
7shi <7shi@live.jp>
2010-07-08 15:05:57 +0900

    remove main

--- Test/Program.fs
+++ Test/Program.fs
@@ -6,32 +6,31 @@ open System.Windows.Forms
 
 open ELF
 
-let main() =
-    let f = new Form()
-    let mono = new Font(FontFamily.GenericMonospace, f.Font.Size)
-    let t = new TextBox(Multiline = true, ScrollBars = ScrollBars.Both,
-                        Dock = DockStyle.Fill, WordWrap = false, Font = mono)
-    let m = new MainMenu()
-    let mi = new MenuItem("開く")
-    mi.Click.Add <| fun _ ->
-        use ofd = new OpenFileDialog()
-        if ofd.ShowDialog() = DialogResult.OK then
-            use fs = new FileStream(ofd.FileName, FileMode.Open)
-            use br = new BinaryReader(fs)
-            let sb = new StringBuilder()
-            ignore <| sb.AppendLine("*** Betelgeuse - Alpha Orionis ***")
-            ignore <| sb.AppendLine()
-            try
-                ignore <| ELF64.Read sb br
-            with ex ->
-                ignore <| sb.AppendLine(ex.Message)
-            t.Text <- sb.ToString()
-    ignore <| m.MenuItems.Add mi
-    f.Menu <- m
-    f.Controls.Add t
-    f.Show()
-    
-    Application.Run(f)
+let f = new Form()
+let mono = new Font(FontFamily.GenericMonospace, f.Font.Size)
+let t = new TextBox(Multiline = true, ScrollBars = ScrollBars.Both,
+                    Dock = DockStyle.Fill, WordWrap = false, Font = mono)
+f.Controls.Add t
+
+let open'(fn:string) =
+    use fs = new FileStream(fn, FileMode.Open)
+    use br = new BinaryReader(fs)
+    let sb = new StringBuilder()
+    ignore <| sb.AppendLine("*** Betelgeuse - Alpha Orionis ***")
+    ignore <| sb.AppendLine()
+    try
+        ignore <| ELF64.Read sb br
+    with ex ->
+        ignore <| sb.AppendLine(ex.Message)
+    t.Text <- sb.ToString()
+
+let m = new MainMenu()
+let mi = new MenuItem("開く")
+mi.Click.Add <| fun _ ->
+    use ofd = new OpenFileDialog()
+    if ofd.ShowDialog() = DialogResult.OK then open' ofd.FileName
+ignore <| m.MenuItems.Add mi
+f.Menu <- m
 
 [<STAThread>]
-do main()
+do Application.Run f
