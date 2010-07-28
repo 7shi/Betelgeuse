2bb869cafd88a1cbfa4915839af9cb1977c0d881
7shi <7shi@live.jp>
2010-07-28 20:49:18 +0900

    make *printf() syscalls

--- Betelgeuse/Alpha.Syscall.fs
+++ Betelgeuse/Alpha.Syscall.fs
@@ -2,6 +2,7 @@
 
 open System
 open System.IO
+open System.Text
 
 open Alpha
 open Alpha.Memory
@@ -32,26 +33,26 @@ let getSlot (f:int) =
 let exit (vm:VM) =
     vm.pc <- stackEnd
 
-let fputc (vm:VM) =
-    vm.v0 <-
-        let c = vm.a0 |> char
-        let f = vm.a1 |> int
-        if f = 0 then
-            if c = '\n' then
-                vm.out.WriteLine()
-            else
-                vm.out.Write("{0}", c)
-            1UL
+let _fputc (vm:VM) (ch:byte) (f:int) =
+    if f = 0 then
+        if ch = byte('\n') then
+            vm.out.WriteLine()
         else
-            let fs = getSlot f
-            if box fs = null then uint64(-1)
-            else
-                try
-                    if c = '\n' && (attrs.[f] &&& aText) <> 0 then
-                        fs.WriteByte ('\r' |> byte)
-                    fs.WriteByte (c |> byte)
-                    1UL
-                with _ -> uint64(-1)
+            vm.out.Write("{0}", ch |> char)
+        1UL
+    else
+        let fs = getSlot f
+        if box fs = null then uint64(-1)
+        else
+            try
+                if ch = byte('\n') && (attrs.[f] &&& aText) <> 0 then
+                    fs.WriteByte ('\r' |> byte)
+                fs.WriteByte ch
+                1UL
+            with _ -> uint64(-1)
+
+let fputc (vm:VM) =
+    vm.v0 <- _fputc vm (vm.a0 |> byte) (vm.a1 |> int)
 
 let fgetc (vm:VM) =
     vm.v0 <-
@@ -169,6 +170,128 @@ let fseek (vm:VM) =
                     1UL
             with _ -> 1UL
 
+let _fsnputc (vm:VM) (ch:byte) (f:int) (psb:uint64[]) (plen:uint64[]) =
+    if psb = null then
+        _fputc vm ch f |> ignore
+        1
+    else
+        let len = plen.[0]
+        if len = 0UL then 0
+        else
+            plen.[0] <- len - 1UL
+            let sb = psb.[0]
+            psb.[0] <- sb + 1UL
+            if len = 1UL then
+                write8 vm sb 0uy
+                0
+            else
+                write8 vm sb ch
+                1
+
+let _fsnprintstr (vm:VM) (buf:byte[]) (ptr:int) (f:int) (psb:uint64[]) (plen:uint64[]) =
+    if f = 0 && psb = null then
+        let rec count p =
+            if buf.[ptr + p] = 0uy then p else count (p + 1)
+        let len = count 0
+        vm.out.Write(Encoding.UTF8.GetString(buf, ptr, len))
+        len
+    else
+        let rec write p =
+            let ch = buf.[ptr + p]
+            if ch = 0uy || (_fsnputc vm ch f psb plen) = 0 then p
+            else write (p + 1)
+        write 0
+
+let _fsnprintlong (vm:VM) (v:int64) (w:int) (zero:bool) (f:int) (psb:uint64[]) (plen:uint64[]) =
+    let str =
+        if w = 0 then
+            v.ToString()
+        else if zero then
+            String.Format("{0:d" + w.ToString() + "}", v)
+        else
+            String.Format("{0," + w.ToString() + "}", v)
+    let buf = Encoding.UTF8.GetBytes(str + "\u0000")
+    _fsnprintstr vm buf 0 f psb plen
+
+let _fsnprinthex (vm:VM) (v:uint64) (w:int) (zero:bool) (f:int) (psb:uint64[]) (plen:uint64[]) =
+    let str =
+        if w = 0 then
+            v.ToString("x")
+        else if zero then
+            String.Format("{0:x" + w.ToString() + "}", v)
+        else
+            String.Format("{0," + w.ToString() + ":x}", v)
+    let buf = Encoding.UTF8.GetBytes(str + "\u0000")
+    _fsnprintstr vm buf 0 f psb plen
+
+let _parseint (buf:byte[]) (ptr:int) =
+    let rec parse b p =
+        let ch = buf.[ptr + p]
+        if ch < byte('0') || ch > byte('9') then b, p
+        else parse (b * 10 + int(ch - byte('0'))) (p + 1)
+    parse 0 0
+
+let _vfsnprintf (vm:VM) (f:int) (psb:uint64[]) (plen:uint64[]) (format:uint64) (args:uint64[]) stack =
+    let get_arg n =
+        if n < args.Length then args.[n] else read64 vm (vm.sp + uint64((n - args.Length + stack) * 8))
+    let mp = getPtr vm format 1
+    let rec write len p arg =
+        let ch = mp.buf.[mp.ptr + p]
+        let da, dp, dl =
+            match ch |> char with
+            | '\u0000' ->
+                if psb <> null then _fsnputc vm ch f psb plen |> ignore
+                0, 0, 0
+            | '%' ->
+                let ch2 = mp.buf.[mp.ptr + p + 1]
+                match ch2 |> char with
+                | 'd' ->
+                    1, 2, _fsnprintlong vm (int64 (get_arg arg)) 0 false f psb plen
+                | 'x' ->
+                    1, 2, _fsnprinthex vm (get_arg arg) 0 false f psb plen
+                | 'p' ->
+                    1, 2, (_fsnprintstr vm [| byte('0'); byte('x'); 0uy |] 0 f psb plen) +
+                          (_fsnprinthex vm (get_arg arg) 16 true f psb plen)
+                | 'c' ->
+                    1, 2, _fsnputc vm (byte (get_arg arg)) f psb plen
+                | 's' ->
+                    let mp = getPtr vm (get_arg arg) 1
+                    1, 2, _fsnprintstr vm mp.buf mp.ptr f psb plen
+                | '%' ->
+                    _fsnputc vm ch f psb plen |> ignore
+                    0, 2, 1
+                | '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' ->
+                    let zero = ch2 = byte('0')
+                    let w, wlen = _parseint mp.buf (mp.ptr + p + 1)
+                    let ch3 = mp.buf.[mp.ptr + p + 1 + wlen]
+                    match ch3 |> char with
+                    | 'd' ->
+                        1, 2 + wlen, _fsnprintlong vm (int64 (get_arg arg)) w zero f psb plen
+                    | 'x' ->
+                        1, 2 + wlen, _fsnprinthex vm (get_arg arg) w zero f psb plen
+                    | _ ->
+                        _fsnputc vm ch f psb plen |> ignore
+                        0, 1, 1
+                | _ ->
+                    _fsnputc vm ch f psb plen |> ignore
+                    0, 1, 1
+            | _ ->
+                _fsnputc vm ch f psb plen |> ignore
+                0, 1, 1
+        if dl = 0 then len else write (len + dl) (p + dp) (arg + da)
+    write 0 0 0 |> uint64
+
+let printf (vm:VM) =
+    vm.v0 <- _vfsnprintf vm 0 null null vm.a0 [| vm.a1; vm.a2; vm.a3; vm.a4; vm.a5 |] 0
+
+let fprintf (vm:VM) =
+    vm.v0 <- _vfsnprintf vm (int vm.a0) null null vm.a1 [| vm.a2; vm.a3; vm.a4; vm.a5 |] 0
+
+let snprintf (vm:VM) =
+    let psb = [| vm.a0 |]
+    let plen = [| vm.a1 |]
+    vm.v0 <- _vfsnprintf vm 0 psb plen vm.a2 [| vm.a3; vm.a4; vm.a5 |] 2
+
 let funcs =
     [| exit
        fputc
@@ -177,7 +300,10 @@ let funcs =
        fclose
        fwrite
        fread
-       fseek |]
+       fseek
+       printf
+       fprintf
+       snprintf |]
 
 let funcStart = 0x00ef0000UL
 let funcEnd = funcStart + uint64(funcs.Length * 4)
--- Silverlight/Test/6.c
+++ Silverlight/Test/6.c
@@ -4,7 +4,7 @@ typedef long int64_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
 typedef unsigned long uint64_t;
-
+typedef unsigned long size_t;
 typedef void FILE;
 
 void (*exit)(int) = (void *)0x00ef0000;
@@ -12,17 +12,19 @@ int (*fputc)(int, FILE *) = (void *)0x00ef0004;
 int (*fgetc)(FILE *) = (void *)0x00ef0008;
 FILE *(*fopen)(const char *, const char *) = (void *)0x00ef000c;
 int (*fclose)(FILE *) = (void *)0x00ef0010;
-int (*fwrite)(const void *, int, int, FILE *) = (void *)0x00ef0014;
-int (*fread)(void *, int, int, FILE *) = (void *)0x00ef0018;
+int (*fwrite)(const void *, size_t, size_t, FILE *) = (void *)0x00ef0014;
+int (*fread)(void *, size_t, size_t, FILE *) = (void *)0x00ef0018;
 int (*fseek)(FILE *, long, int) = (void *)0x00ef001c;
+int (*printf)(const char *, ...) = (void *)0x00ef0020;
+int (*fprintf)(FILE *, const char *, ...) = (void *)0x00ef0024;
+int (*snprintf)(char *, size_t, const char *, ...) = (void *)0x00ef0028;
 
-int printf(const char *, ...);
-int fprintf(FILE *, const char *, ...);
-int snprintf(char *, int, const char *, ...);
 int strcmp(const char *, const char *);
-char *strncpy(char *, const char *, int);
-char *strncat(char *, const char *, int);
-void *memset(void *, int, int);
+char *strncpy(char *, const char *, size_t);
+char *strncat(char *, const char *, size_t);
+size_t strlen(const char *);
+void *memcpy(void *, const void *, size_t);
+void *memset(void *, int, size_t);
 
 /* -------------------------------- */
 
@@ -59,177 +61,7 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-/* -------------------------------- */
-
-void fsnputc(int ch, FILE *f, char **sb, int *len)
-{
-    if (sb)
-    {
-        if ((*len)-- > 0) *((*sb)++) = *len ? ch : 0;
-    }
-    else
-        fputc(ch, f);
-}
-
-int fsnprintstr(const char *s, FILE *f, char **sb, int *len)
-{
-    int ret = 0;
-    for (; *s; s++, ret++) fsnputc(*s, f, sb, len);
-    return ret;
-}
-
-int fsnprintlong(long v, int w, char pad, FILE *f, char **sb, int *len)
-{
-    char buf[32];
-    char *p, *start;
-    unsigned long uv = (unsigned long)v;
-    int ret = 0;
-    if (v < 0)
-    {
-        fsnputc('-', f, sb, len);
-        ret++;
-        uv = 0 - uv;
-    }
-    p = buf + sizeof(buf) - 1;
-    *p = '\0';
-    if (v == 0)
-        *(--p) = '0';
-    else
-        for (; uv; uv /= 10)
-            *(--p) = '0' + (uv % 10);
-    if (w < 0) w = 0;
-    if (w > sizeof(buf) - 1) w = sizeof(buf) - 1;
-    start = buf + sizeof(buf) - 1 - w;
-    while (p > start) *(--p) = pad;
-    return ret + fsnprintstr(p, f, sb, len);
-}
-
-int fsnprinthex(unsigned long v, int w, char pad, FILE *f, char **sb, int *len)
-{
-    char buf[32];
-    char *p, *start;
-    int ret = 0;
-    p = buf + sizeof(buf) - 1;
-    *p = '\0';
-    if (v == 0)
-        *(--p) = '0';
-    else
-        for (; v; v >>= 4)
-            *(--p) = "0123456789abcdef"[v & 15];
-    if (w < 0) w = 0;
-    if (w > sizeof(buf) - 1) w = sizeof(buf) - 1;
-    start = buf + sizeof(buf) - 1 - w;
-    while (p > start) *(--p) = pad;
-    return ret + fsnprintstr(p, f, sb, len);
-}
-
-int parseint(const char **p)
-{
-    int ret = 0;
-    for (; **p; (*p)++)
-    {
-        char ch = **p;
-        if ('0' <= ch && ch <= '9')
-        {
-            ret *= 10;
-            ret += ch - '0';
-        }
-        else
-            break;
-    }
-    return ret;
-}
-
-int vfsnprintf(FILE *f, char **sb, int *len, const char *format, void **args)
-{
-    const char *p = format, *pp;
-    int ret = 0, n, pad;
-    for (; *p; p++)
-    {
-        if (*p == '%')
-        {
-            pp = p;
-            switch (*(++p))
-            {
-            case '\0':
-                fsnputc('%', f, sb, len);
-                ret++;
-                p--;
-                break;
-            case 'd':
-                ret += fsnprintlong(*(long *)(args++), 0, '0', f, sb, len);
-                break;
-            case 'x':
-                ret += fsnprinthex(*(unsigned long *)(args++), 0, '0', f, sb, len);
-                break;
-            case 'p':
-                fsnprintstr("0x", f, sb, len);
-                ret += fsnprinthex(*(unsigned long *)(args++), 16, '0', f, sb, len) + 2;
-                break;
-            case 'c':
-                fsnputc(*(char *)(args++), f, sb, len);
-                ret++;
-                break;
-            case 's':
-                ret += fsnprintstr(*(const char **)(args++), f, sb, len);
-                break;
-            case '%':
-                fsnputc('%', f, sb, len);
-                ret++;
-                break;
-            case '0':
-            case '1':
-            case '2':
-            case '3':
-            case '4':
-            case '5':
-            case '6':
-            case '7':
-            case '8':
-            case '9':
-                pad = *p == '0' ? '0' : ' ';
-                n = parseint(&p);
-                switch (*p)
-                {
-                case 'd':
-                    ret += fsnprintlong(*(long *)(args++), n, pad, f, sb, len);
-                    break;
-                case 'x':
-                    ret += fsnprinthex(*(unsigned long *)(args++), n, pad, f, sb, len);
-                    break;
-                default:
-                    for (; pp <= p; pp++, ret++) fsnputc(*pp, f, sb, len);
-                    break;
-                }
-                break;
-            default:
-                for (; pp <= p; pp++, ret++) fsnputc(*pp, f, sb, len);
-                break;
-            }
-        }
-        else
-        {
-            fsnputc(*p, f, sb, len);
-            ret++;
-        }
-    }
-    return ret;
-}
-
-int printf(const char * format, ...)
-{
-    return vfsnprintf(0, 0, 0, format, ((void **)&format) + 9);
-}
-
-int fprintf(FILE *f, const char *format, ...)
-{
-    return vfsnprintf(f, 0, 0, format, ((void **)&format) + 9);
-}
-
-int snprintf(char *sb, int len, const char *format, ...)
-{
-    return vfsnprintf(0, &sb, &len, format, ((void **)&format) + 11);
-}
+/* libc implementation */
 
 int strcmp(const char *a, const char *b)
 {
@@ -240,7 +72,7 @@ int strcmp(const char *a, const char *b)
     return 0;
 }
 
-char *strncpy(char *dst, const char *src, int size)
+char *strncpy(char *dst, const char *src, size_t size)
 {
     for (; size > 0; size--, dst++, src++)
     {
@@ -250,14 +82,30 @@ char *strncpy(char *dst, const char *src, int size)
     return dst;
 }
 
-char *strncat(char *dst, const char *src, int size)
+char *strncat(char *dst, const char *src, size_t size)
 {
     for (; size > 0 && *dst; size--, dst++);
     strncpy(dst, src, size);
     return dst;
 }
 
-void *memset(void *dst, int c, int len)
+size_t strlen(const char *s)
+{
+    size_t ret = 0;
+    for (; *s; s++, ret++);
+    return ret;
+}
+
+void *memcpy(void *dst, const void *src, size_t size)
+{
+    char *d = (char *)dst;
+    const char *s = (const char *)src;
+    for (; size > 0; size--, d++, s++)
+        *d = *s;
+    return dst;
+}
+
+void *memset(void *dst, int c, size_t len)
 {
     char *d = (char *)dst;
     int i;
